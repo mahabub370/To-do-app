1a6141674c1ce9480ae87e4d91e9b79c223d76a2
mahabub370 <mahabub370@yahoo.com>
2024-08-21 23:29:47 +0600

    html and css is done now adding some js

---
+++ main.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+
+function addTask(){
+    if(inputBox.value === ""){
+        alert("You must add your task!")
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+    }
+};
